e1086cacf6bba92e4b1bfdefda1ab8c6d5fd24f4
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 13:09:41 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -42,5 +42,5 @@ function displayCommits() {
         '</li>'
     )
     .join('')}</ul>`;
-  document.getElementById('commits').innerHTML = commitsList;
+  document.getElementById('details').innerHTML = commitsList;
 }
