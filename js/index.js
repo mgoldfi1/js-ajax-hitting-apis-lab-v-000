d12a75fbccfe05c8d5783b2f0210722a1502638f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 13:38:13 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -8,7 +8,7 @@ function getRepositories() {
 function displayRepositories() {
   var repos = JSON.parse(this.responseText);
   console.log(repos);
-  const repoList = `<ul>${repos
+  var repoList = `<ul>${repos
     .map(
       r =>
         '<li>' +
@@ -24,7 +24,7 @@ function displayRepositories() {
 
 function getCommits(el) {
   const name = el.dataset.repo;
-  
+
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
   req.open('GET', `https://api.github.com/repos/${document.getElementById('username').value}/` + name + '/commits');
