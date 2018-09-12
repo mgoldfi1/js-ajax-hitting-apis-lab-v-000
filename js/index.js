79e6b4de3317b4f0b63a3fdea9e85c998aa5d374
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 13:31:57 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -26,7 +26,7 @@ function getCommits(el) {
   const name = el.dataset.repo;
   const req = new XMLHttpRequest();
   req.addEventListener('load', displayCommits);
-  req.open('GET', `https://api.github.com/repos/${document.getElementById('username').value}/` + name + '/git/commits');
+  req.open('GET', `https://api.github.com/repos/${document.getElementById('username').value}/` + name + '/commits');
   req.send();
 }
 
