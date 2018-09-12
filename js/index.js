70680b7c6f6df2fb5a36583b481a53c141fa6cb0
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 12:58:36 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -1,4 +1,6 @@
 // your code here
 function getRepositories() {
-
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', displayRepositories);
+  req.open('GET', `https://api.github.com/users/${document.getElementById('username').value}/repos`)
 }
