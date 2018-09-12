9e37d7acb13d00142e35f6a021784c4882544e2f
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 13:45:04 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -32,7 +32,6 @@ function getCommits(el) {
 
 function displayCommits() {
   const commits = JSON.parse(this.responseText);
-   console.log(commits[0].author)
   const commitsList = `<ul>${commits
     .map(
       commit =>
