de510cf721b40028fffbeaf0b81b8a7d522bd4b5
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 14:12:48 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -56,7 +56,7 @@ function getBranches(el) {
 
 function displayBranches() {
   const branches = JSON.parse(this.responseText);
-  const branchesList = `<ul>${commits
+  const branchesList = `<ul>${branches
     .map(
       commit =>
         '<li><strong>' +
