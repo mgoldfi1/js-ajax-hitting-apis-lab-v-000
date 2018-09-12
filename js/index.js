e492a1904c1d0ab6521934dc77a45accb7e49291
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 13:51:19 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -36,7 +36,7 @@ function displayCommits() {
     .map(
       commit =>
         '<li><strong>' +
-        commit.author.login +
+        commit.author.login + ' ' + commit.author.name
         '</strong> - ' +
         commit.commit.message +
         '</li>'
