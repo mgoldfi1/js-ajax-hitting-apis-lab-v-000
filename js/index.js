cc19696110a7f9ad8280641217e97c738aa78bc8
Matthew Goldfine <mgoldfi1@binghamton.edu>
2018-09-12 13:05:46 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -20,3 +20,12 @@ function displayRepositories() {
     .join('')}</ul>`;
   document.getElementById('repositories').innerHTML = repoList;
 }
+
+
+function getCommits(el) {
+  const name = el.dataset.repo;
+  const req = new XMLHttpRequest();
+  req.addEventListener('load', showCommits);
+  req.open('GET', 'https://api.github.com/repos/octocat/' + name + '/commits');
+  req.send();
+}
